602350e122fbfb559f12d61ca7fffce7281030a8
Leya Wehner <leya.wehner@proton.me>
2025-02-14 15:51:02 +0100

    Added comments

--- sysmond.c
+++ sysmond.c
@@ -1,8 +1,13 @@
 /*
-sysmond.c implementiert ein erweiterbares Monitoring System für verschiedene Messwerte und agiert nach der Initialisierung der GPIOs und dem Device Tree als Daemon im Hintergrund.
+sysmond.c
+
+Implementiert ein erweiterbares Monitoring System für verschiedene Messwerte und agiert nach der Initialisierung der GPIOs und dem Device Tree als Daemon im Hintergrund.
 Für jeden erhobenen Messwert wird ein Kindprozess vom Prozess abgespalten und zwei Pipes für bidirektionale Kommunikation erzeugt.
 Die Kindprozesse erheben ihre Messwerte und warten dann auf ein Signal des Elternprozesses um ihre Messdaten an diesen zu übermitteln.
 Der Elternprozess schreibt die Daten dann in das für die Kommunikation zwischen Userspace und Kernel genutzte Proc-File unter /proc/monitoring-system
+
+Alle Funktionen wurden von uns beiden (Leya Wehner und Julian Frank) in Kollaboration geschreiben.
+Die Kommentare zur Funktionsbeschreibung befinden sich über der jeweiligen Funktion.
 */
 
 #include <stdio.h>
@@ -24,19 +29,22 @@ Der Elternprozess schreibt die Daten dann in das für die Kommunikation zwischen
 #define BUFFER_SIZE 1024
 #define DEVICE_FILE "/proc/monitoring-system"
 
-
+//  Array das die Befehle zum auslesen der jeweiligen Messwerte enthält. Die von den Befehlen zurückgegebenen Werte müssen in einen uint16_t konvertierbar sein.
 const char *jobs[NUM_JOBS] = {
-    "top -bn1 | grep \"Cpu(s)\" | sed \"s/.*, *\\([0-9.]*\\)%* id.*/\\1/\" | awk '{printf \"%dn\", 100 - $1}'", // CPU usage
-    "cat /sys/class/thermal/thermal_zone0/temp", // CPU temperature
-    "free | grep Mem | awk '{printf \"%dn\", $3 / $2 * 100}'", // Memory usage
+    "top -bn1 | grep \"Cpu(s)\" | sed \"s/.*, *\\([0-9.]*\\)%* id.*/\\1/\" | awk '{printf \"%dn\", 100 - $1}'", // CPU Nutzung (in Prozent)
+    "cat /sys/class/thermal/thermal_zone0/temp", // CPU Temperatur (in Miligrad Celsius)
+    "free | grep Mem | awk '{printf \"%dn\", $3 / $2 * 100}'", // Speichernutzung (in Prozent)
 };
 
+/*
+    Enthält die Logik für die Kindprozesse, welche die Messwerte kontinuierlich auslesen und anschließend an den Parent zurücksenden.
+    Die Kommunikation erfolgt hierbei über jeweils 2 Pipes um bidirektionale Kommunikation zu ermöglichen.
+*/
 void child_process(int write_fd, int read_fd, const char *command) {
-    // Parent collects output
     struct sched_param param;
-    param.sched_priority = 98;  // Highest real-time priority
+    param.sched_priority = 98;
 
-    // Set the parent process to real-time priority
+    // Setzt den Kindprozess auf Realtime Priorität 98
     if (sched_setscheduler(0, SCHED_FIFO, &param) == -1) {
         perror("sched_setscheduler failed");
         exit(EXIT_FAILURE);
@@ -44,20 +52,17 @@ void child_process(int write_fd, int read_fd, const char *command) {
 
     char buffer[BUFFER_SIZE];
     uint8_t signal;
-    uint32_t val;
+    uint16_t val;
     FILE *fp;
 
-    // Continuous execution loop
     while (1) {
         signal = 0;
 
-        // Execute command and open pipe to read its output
         fp = popen(command, "r");
         if (fp == NULL) {
             val = 0;
         }
         else {
-            // Read command output and send to parent
             fgets(buffer, BUFFER_SIZE, fp);
             val = atoi(buffer);
         }
@@ -71,45 +76,14 @@ void child_process(int write_fd, int read_fd, const char *command) {
     }
 }
 
-int main() {
-    int message_pipes[NUM_JOBS][2];
-    int signal_pipes[NUM_JOBS][2];
-
-    pid_t pids[NUM_JOBS];
-
-    for (int i = 0; i < NUM_JOBS; i++) {
-        if (pipe(message_pipes[i]) == -1) {
-            perror("pipe failed");
-            exit(EXIT_FAILURE);
-        }
-
-        if (pipe(signal_pipes[i]) == -1) {
-            perror("pipe failed");
-            exit(EXIT_FAILURE);
-        }
-
-        pids[i] = fork();
-        if (pids[i] < 0) {
-            perror("fork failed");
-            exit(EXIT_FAILURE);
-        }
-
-        if (pids[i] == 0) {  // Child process
-            close(message_pipes[i][0]);
-            close(signal_pipes[i][1]);
-            child_process(message_pipes[i][1], signal_pipes[i][0], jobs[i]);
-            exit(EXIT_SUCCESS);
-        }
-
-        close(message_pipes[i][1]);
-        close(signal_pipes[i][0]);
-    }
-
-    // Parent collects output
+/*
+    Enthält die Logik für den Elternprozess, welcher die Werte die von den Kindprozessen ausgelesen und über die Pipes geschickt wurden aggregiert und an das Kernelmodul übermittelt.
+*/
+int parent_process(int message_pipes[][2], int signal_pipes[][2]) {
     struct sched_param param;
     param.sched_priority = 99;  // Highest real-time priority
 
-    // Set the parent process to real-time priority
+    // Setzt den Kindprozess auf Realtime Priorität 99
     if (sched_setscheduler(0, SCHED_FIFO, &param) == -1) {
         perror("sched_setscheduler failed");
         return EXIT_FAILURE;
@@ -119,12 +93,12 @@ int main() {
 
     fd = open(DEVICE_FILE, O_WRONLY);
     if (fd < 0) {
-        perror("Fehler beim Öffnen der Datei");
+        perror("Error opening the file");
         exit(EXIT_FAILURE);
     }
 
     uint8_t buffer[1 + NUM_JOBS + 2 * NUM_JOBS];
-    uint32_t val;
+    uint16_t val;
     uint8_t one = 1;
 
     buffer[0] = 0xC2; // Sender ID
@@ -158,13 +132,51 @@ int main() {
         }
 
         if (write(fd, buffer, sizeof(buffer)) < 0) {
-            perror("Fehler beim Schreiben");
+            perror("Error writing to the file");
         }
 
         clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &next, NULL);
     }
-
-    // Should not reach here as children run infinitely
     close(fd);
     return 0;
+}
+
+/* 
+    Die Funktion dient als Einstiegspunkt für das Programm und spaltet vom Elternprozess für jeden überwachten Wert jeweils einen Kindprozess ab.
+*/
+int main() {
+    int message_pipes[NUM_JOBS][2];
+    int signal_pipes[NUM_JOBS][2];
+
+    pid_t pids[NUM_JOBS];
+
+    for (int i = 0; i < NUM_JOBS; i++) {
+        if (pipe(message_pipes[i]) == -1) {
+            perror("pipe failed");
+            exit(EXIT_FAILURE);
+        }
+
+        if (pipe(signal_pipes[i]) == -1) {
+            perror("pipe failed");
+            exit(EXIT_FAILURE);
+        }
+
+        pids[i] = fork();
+        if (pids[i] < 0) {
+            perror("fork failed");
+            exit(EXIT_FAILURE);
+        }
+
+        if (pids[i] == 0) {  // Kindprozess
+            close(message_pipes[i][0]);
+            close(signal_pipes[i][1]);
+            child_process(message_pipes[i][1], signal_pipes[i][0], jobs[i]);
+            exit(EXIT_SUCCESS);
+        }
+
+        close(message_pipes[i][1]);
+        close(signal_pipes[i][0]);
+    }
+
+    return parent_process(message_pipes, signal_pipes);
 }
